7c271aea4aef0c0c17e453eee58c239f88aa60ee
Jay Parmar <wrteam.jay@gmail.com>
2025-03-18 15:39:36 +0530

    chore: added gitignore and deleted unnecessary files

--- .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
+++
@@ -1,5 +0,0 @@
-import lunr from "/Users/wrteam/Flutter/projects/common_app_doc/node_modules/lunr/lunr.js";
-export const removeDefaultStopWordFilter = false;
-export const language = ["en"];
-export const searchIndexUrl = "search-index{dir}.json";
-export const searchResultLimits = 8;
--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/wrteam/Flutter/projects/common_app_doc/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/wrteam/Flutter/projects/common_app_doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/wrteam/Flutter/projects/common_app_doc/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/wrteam/Flutter/projects/common_app_doc/src/css/custom.css"),
-];
